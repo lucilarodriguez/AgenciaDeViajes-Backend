ffdc7f2d49982b08ee7f8cefe706b6aade13fab4
Yani044 <gonzalezyanina044@gmail.com>
2020-03-05 07:10:13 -0300

    Actualizacion mercadopago

--- agenciabackend/src/main/java/com/example/demo/controladores/DetalleReservaControlador.java
+++ agenciabackend/src/main/java/com/example/demo/controladores/DetalleReservaControlador.java
@@ -1,7 +1,9 @@
 
 package com.example.demo.controladores;
 
+import com.example.demo.entidades.DetalleReserva;
 import com.example.demo.entidades.Producto;
+import com.example.demo.servicios.DetalleReservaService;
 import com.example.demo.servicios.ProductoService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,24 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
-public class ProductoControlador {
-    
-    @Autowired
-    private ProductoService productoService;
-    
-    @CrossOrigin(origins = "http://localhost:4200")
-    @GetMapping("/productos")
-    public List<Producto> index(ModelMap modelo) {
-        List<Producto> productos = productoService.obtenerProductos();
-        
-        return productos;
+public class DetalleReservaControlador {
+
+    public static List<DetalleReserva> obtenerDetalleReserva() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
+    @Autowired
+    private DetalleReservaService  detalleReservaService;
     
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping("/detallereserva")
+    public List<DetalleReserva> index(ModelMap modelo) {
+        List<DetalleReserva> detalle = (List<DetalleReserva>) detalleReservaService;
         
-        
-        
-    
-    
-    
+        return detalle;
+    }
 }
--- agenciabackend/src/main/java/com/example/demo/servicios/PagoService.java
+++ agenciabackend/src/main/java/com/example/demo/servicios/PagoService.java
@@ -5,32 +5,41 @@
  */
 package com.example.demo.servicios;
 
+import com.example.demo.controladores.ProductoControlador;
+import com.example.demo.entidades.DetalleReserva;
 import com.mercadopago.MercadoPago;
 import com.mercadopago.exceptions.MPConfException;
 import com.mercadopago.exceptions.MPException;
 import com.mercadopago.resources.Preference;
 import com.mercadopago.resources.datastructures.preference.Item;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 
 
 public class PagoService {
     
   public void pagar() throws MPConfException, MPException{
-      
+      PagoService ps= new PagoService();
+      //DetalleReservaService detallereservaservice = new DetalleReservaService();
+      ProductoControlador pc= new ProductoControlador();
         // Agrega credenciales
-        MercadoPago.SDK.setAccessToken("PROD_ACCESS_TOKEN");
-
+        MercadoPago.SDK.setAccessToken("ACCESS_TOKEN");
+        
         // Crea un objeto de preferencia
         Preference preference = new Preference();
 
         // Crea un ítem en la preferencia
         Item item = new Item();
-        item.setTitle("Mi producto")
+        item.setTitle(ps.titulo())
             .setQuantity(1)
-            .setUnitPrice((float) 75.56);
+            .setUnitPrice((float)ps.precioFinal() );
         preference.appendItem(item);
-        preference.save();
-
+        if(preference.setAutoReturn(Preference.AutoReturn.approved).equals(true)){
+            //GUARDAR EN BASE DE DATOS
+        }
+        
         /*   PARA AGREGAR AL BOTON PAGAR
         <form action="/procesar-pago" method="POST">
   <script
@@ -42,7 +51,26 @@ public class PagoService {
       
   }
   
-
+  public float precioFinal ( ){
+     DetalleReservaService detalleReservaService= new DetalleReservaService();
+      
+      List <DetalleReserva> a= detalleReservaService.obtenerDetalleReserva();
+      float preciofinal=0;
+      for (DetalleReserva detalleReserva : a) {
+          preciofinal += detalleReserva.getCantidadDePersonas()*detalleReserva.getProducto().getPrecio();
+      }
+      return preciofinal;
+  }
+    public String titulo ( ){
+     DetalleReservaService detalleReservaService= new DetalleReservaService();
+      
+      List <DetalleReserva> a= detalleReservaService.obtenerDetalleReserva();
+      String titulo="";
+      for (DetalleReserva detalleReserva : a) {
+          titulo += detalleReserva.getProducto().getNombre()+" X "+detalleReserva.getCantidadDePersonas().toString()+ "/ ";
+      }
+      return titulo;
+  }
     
 }
 
